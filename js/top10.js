88b3f049c9f087b428463dc38993f56aed4d6fed
JuanManuelCalle <114536103+JuanManuelCalle@users.noreply.github.com>
2023-07-15 15:42:05 -0500

    Se corrije nombre de indes a index y se sube top10

---
+++ js/top10.js
@@ -0,0 +1,63 @@
+export const artistas = [
+    {
+      nombre: "The Beatles",
+      cancion: "Hey Jude",
+      urlImg: "https://img2.rtve.es/i/?w=1600&i=1565264925747.jpg",
+      urlCancion: "https://www.youtube.com/watch?v=A_MjCqQoLLA"
+    },
+    {
+      nombre: "Elvis Presley",
+      cancion: "Suspicious Minds",
+      urlImg: "https://static.eldiario.es/clip/f122328c-e6a7-4bec-a15e-05e43ef0e7f3_16-9-discover-aspect-ratio_default_0.jpg",
+      urlCancion: "https://www.youtube.com/watch?v=WrMGGouem3c&ab_channel=ElvisPresleyVEVO"
+    },
+    {
+      nombre: "Michael Jackson",
+      cancion: "Thriller",
+      urlImg: "https://www.eltiempo.com/files/article_main_1200/uploads/2023/04/18/643f111cbeed0.jpeg",
+      urlCancion: "https://www.youtube.com/watch?v=sOnqjkJTMaA"
+    },
+    {
+      nombre: "Madonna",
+      cancion: "Like a Prayer",
+      urlImg: "https://images.hellomagazine.com/horizon/43/bb4a5daec3e1-madonna-home.jpg",
+      urlCancion: "https://www.youtube.com/watch?v=79fzeNUqQbQ"
+    },
+    {
+      nombre: "Queen",
+      cancion: "Bohemian Rhapsody",
+      urlImg: "https://historia-biografia.com/wp-content/uploads/2018/11/queen.webp",
+      urlCancion: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ"
+    },
+    {
+      nombre: "ABBA",
+      cancion: "Dancing Queen",
+      urlImg: "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2018/04/ABBA-scaled.jpg?fit=2560%2C1440&quality=50&strip=all&ssl=1",
+      urlCancion: "https://www.youtube.com/watch?v=xFrGuyw1V8s"
+    },
+    {
+      nombre: "Led Zeppelin",
+      cancion: "Stairway to Heaven",
+      urlImg: "https://cdn.apollo.audio/one/media/63f6/1304/b2b0/1805/a74d/06a1/led-zeppelin-1968-colourised-getty.jpg?quality=80&format=jpg&crop=192,0,1317,1999&resize=crop",
+      urlCancion: "https://www.youtube.com/watch?v=BcL---4xQYA"
+    },
+    {
+      nombre: "Rihanna",
+      cancion: "Umbrella",
+      urlImg: "https://ichef.bbci.co.uk/news/640/cpsprodpb/92CA/production/_128587573_hi082225188.jpg",
+      urlCancion: "https://www.youtube.com/watch?v=CvBfHwUxHIk"
+    },
+    {
+      nombre: "Eminem",
+      cancion: "Lose Yourself",
+      urlImg: "https://www.elespectador.com/resizer/DxY72A8UxznTNa4AkgK2SYAPsXM=/525x350/filters:quality(60):format(jpeg)/cloudfront-us-east-1.images.arcpublishing.com/elespectador/WLOHPQJ6UFBTNLV5CTDTLE2UTA.jpg",
+      urlCancion: "https://www.youtube.com/watch?v=_Yhyp-_hX2s"
+    },
+    {
+      nombre: "Elton John",
+      cancion: "Rocket Man",
+      urlImg: "https://lumiere-a.akamaihd.net/v1/images/g_disneyplusoriginals_eltonjohnlivefarewellfromdodgerss_9ddd0e80.jpeg?region=1,0,1298,730&width=960",
+      urlCancion: "https://www.youtube.com/watch?v=DtVBCG6ThDk"
+    }
+  ];
+  
